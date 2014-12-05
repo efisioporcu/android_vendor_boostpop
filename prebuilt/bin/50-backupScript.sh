0dd74acec5ad6cca518f69a9ea730a1398f80381
Crostantin <crostantin@gmail.com>
2014-12-05 17:07:32 +0100

    Update backup script - big thanks to MrBanKs

--- prebuilt/bin/50-backupScript.sh
+++ prebuilt/bin/50-backupScript.sh
@@ -1,7 +1,10 @@
 #!/sbin/sh
+# 
+# Banks, we want your gapps!!!
 #
-# /system/addon.d/50-cm.sh
-# During a ROM upgrade, this script backs up /system/etc/hosts,
+# /system/addon.d/50-simpleaosp.sh
+# During a SimpleAOSP upgrade, this script backs up /system/etc/hosts,
+# This script will also backup and restore your gapps and other things you add.
 # /system is formatted and reinstalled, then the file is restored.
 #
 
@@ -9,16 +12,8 @@
 
 list_files() {
 cat <<EOF
-etc/paranoid/properties.conf
+etc/hosts
 EOF
-}
-
-check_prereq() {
-export V=  grep revision system/etc/paranoid/properties.conf | cut -c11
-if ( ! grep -q "^ro.papref.revision=$V" /system/build.prop ); then
-  echo "Not backing up files from incompatible version."
-  exit 127
-fi
 }
 
 case "$1" in
